bbcaef17c69ee062f4ff674bfef92cdd9b23ac17
jhu vitoria <jhuliavitoria226@gmail.com>
2024-04-19 17:59:16 -0300

    exercicio concluido!

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿﻿int numero;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+ Console.Write("Digite um número: ");
+ Console.ResetColor();
+
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+ Console.WriteLine("Negativo");
+}
+else if (numero > 0)
+{
+ Console.WriteLine("Positivo");
+}
+else
+{
+ Console.WriteLine("Numero zero");
+}
